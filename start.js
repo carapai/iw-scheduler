2afbdf4a0543cec928ea8d73477e8cb4c4e93834
Charles Olupot <olupotcharles@gmail.com>
2023-06-30 12:54:46 +0300

    added deployment

---
+++ start.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const Runner = require("moleculer").Runner;
+
+const runner = new Runner();
+
+runner
+	.start([
+		process.argv[0],
+		__filename,
+		"--config",
+		path.join(__dirname, "dist/moleculer.config.js"),
+		"--env",
+		path.join(__dirname, "dist/services"),
+	])
+	.catch((err) => {
+		console.error(err.message);
+		process.exit(1);
+	});
